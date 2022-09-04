4090d19548f1ce9abf4f1504ce9066deaba0b923
Navfront <b3rt@yandex.ru>
2022-09-05 00:15:23 +0300

    Рекурсивная стилизация WIP

--- solution.js
+++ solution.js
@@ -1,22 +1,49 @@
 "use strict";
-function createElement({ elementType, content, className }) {
-    return `<${elementType} class="${className !== null && className !== void 0 ? className : ''}">${content}</${elementType}>`;
+function normalizeColor(color) {
+    return `rgb(${String(Math.round(color.r * 255))},${String(Math.round(color.g * 255))},${String(Math.round(color.b * 255))});`;
+}
+function getStyles(node) {
+    var _a, _b, _c, _d, _e, _f;
+    return `${typeof node.layoutMode === 'string' ? 'display:flex;' : ''}
+  ${node.layoutMode === 'VERTICAL' ? 'flex-direction:column;' : ''}
+  ${node.layoutAlign === 'STRETCH' ? 'align-self:stretch;' : ''}
+  ${node.layoutGrow === 0 ? 'flex-grow: 0;' : ''}
+  ${typeof node.itemSpacing === 'number' ? 'gap:'.concat(String(node.itemSpacing), 'px;') : ''}
+  ${(node.backgroundColor != null) ? 'color:'.concat(normalizeColor(node.backgroundColor)) : ''}
+  ${node.primaryAxisAlignItems === 'SPACE_BETWEEN' ? 'justify-content: space-between;' : ''}
+  ${node.counterAxisAlignItems === 'CENTER' ? 'align-items: center;' : ''}
+  ${node.primaryAxisAlignItems === 'CENTER' ? 'justify-content: center;' : ''}
+  ${(node.paddingTop != null) ? 'padding-top:'.concat(String(node.paddingTop), 'px;') : ''}
+  ${(node.paddingBottom != null) ? 'padding-bottom:'.concat(String(node.paddingBottom), 'px;') : ''}
+  ${(node.paddingLeft != null) ? 'padding-left:'.concat(String(node.paddingLeft), 'px;') : ''}
+  ${(node.paddingRight != null) ? 'padding-right:'.concat(String(node.paddingRight), 'px;') : ''}
+  ${typeof ((_a = node.style) === null || _a === void 0 ? void 0 : _a.fontFamily) === 'string' ? 'font-family:'.concat(node.style.fontFamily, ';') : ''}
+  ${typeof ((_b = node.style) === null || _b === void 0 ? void 0 : _b.fontWeight) === 'number' ? 'font-weight:'.concat(String(node.style.fontWeight), ';') : ''}
+  ${typeof ((_c = node.style) === null || _c === void 0 ? void 0 : _c.fontSize) === 'number' ? 'font-size:'.concat(String(node.style.fontSize), 'px;') : ''}
+  ${typeof ((_d = node.style) === null || _d === void 0 ? void 0 : _d.textAlignHorizontal) === 'string' ? 'text-align:'.concat(node.style.textAlignHorizontal.toLowerCase(), ';') : ''}
+  ${typeof ((_e = node.style) === null || _e === void 0 ? void 0 : _e.lineHeightPercent) === 'number' ? 'line-height:'.concat(String(node.style.lineHeightPercent), '%;') : ''}
+  ${(node.background != null) && ((_f = node.background) === null || _f === void 0 ? void 0 : _f.length) > 0 ? 'background-color:'.concat(normalizeColor(node.background[0].color)) : ''}
+
+  `;
+}
+function createElement({ elementType, content, className, styles }) {
+    return `<${elementType} class="${className !== null && className !== void 0 ? className : ''}" style="${styles !== null && styles !== void 0 ? styles : ''}">${content}</${elementType}>`;
 }
 function reducer(node) {
     var _a, _b, _c, _d, _e;
     switch (node.type) {
         case 'DOCUMENT':
-            return createElement({ elementType: 'div', content: (_a = node.characters) !== null && _a !== void 0 ? _a : '', className: node.name.toLowerCase() });
+            return createElement({ elementType: 'div', content: (_a = node.characters) !== null && _a !== void 0 ? _a : '', className: node.name.toLowerCase(), styles: getStyles(node) });
         case 'COMPONENT':
             return '';
         case 'CANVAS':
-            return createElement({ elementType: 'div', content: (_b = node.characters) !== null && _b !== void 0 ? _b : '', className: node.name.toLowerCase() });
+            return createElement({ elementType: 'div', content: (_b = node.characters) !== null && _b !== void 0 ? _b : '', className: node.name.toLowerCase(), styles: getStyles(node) });
         case 'FRAME':
-            return createElement({ elementType: 'div', content: (_c = node.characters) !== null && _c !== void 0 ? _c : '', className: node.name.toLowerCase() });
+            return createElement({ elementType: 'div', content: (_c = node.characters) !== null && _c !== void 0 ? _c : '', className: node.name.toLowerCase(), styles: getStyles(node) });
         case 'TEXT':
-            return createElement({ elementType: 'p', content: (_d = node.characters) !== null && _d !== void 0 ? _d : '', className: node.name.toLowerCase() });
+            return createElement({ elementType: 'span', content: (_d = node.characters) !== null && _d !== void 0 ? _d : '', className: node.name.toLowerCase(), styles: getStyles(node) });
         default:
-            return createElement({ elementType: 'div', content: (_e = node.characters) !== null && _e !== void 0 ? _e : '', className: node.name.toLowerCase() });
+            return createElement({ elementType: 'div', content: (_e = node.characters) !== null && _e !== void 0 ? _e : '', className: node.name.toLowerCase(), styles: getStyles(node) });
     }
 }
 // Парсер
--- solution.ts
+++ solution.ts
@@ -8,20 +8,6 @@ interface Color {
   a: number
 }
 
-interface Style {
-  fontFamily?: 'Inter' | undefined
-  fontPostScriptName?: 'Inter-Bold' | 'Inter-Medium' | undefined
-  fontWeight?: number | undefined
-  textAutoResize?: 'WIDTH_AND_HEIGHT' | 'HEIGHT' | undefined
-  fontSize?: number | undefined
-  textAlignHorizontal?: 'LEFT' | 'CENTER' | 'RIGHT' | undefined
-  textAlignVertical?: 'TOP' | undefined
-  letterSpacing?: number | undefined
-  lineHeightPx?: number | undefined
-  lineHeightPercent?: number | undefined
-  lineHeightUnit?: 'INTRINSIC_%' | undefined
-}
-
 interface Layer {
   blendMode: 'NORMAL'
   visible: boolean
@@ -62,7 +48,7 @@ interface Node {
   name: string
   type: NodeType
   children: Node[] | undefined
-  backgroundColor: Color
+  backgroundColor?: Color
   characters?: string | undefined
   style?: Style | undefined
   background?: Background | undefined
@@ -86,13 +72,27 @@ interface Node {
   counterAxisAlignItems?: 'CENTER' | undefined
   primaryAxisAlignItems?: 'SPACE_BETWEEN' | 'CENTER' | undefined
   strokes?: Layer[] | undefined
+}
 
+interface Style {
+  fontFamily?: 'Inter' | undefined
+  fontPostScriptName?: 'Inter-Bold' | 'Inter-Medium' | undefined
+  fontWeight?: number | undefined
+  textAutoResize?: 'WIDTH_AND_HEIGHT' | 'HEIGHT' | undefined
+  fontSize?: number | undefined
+  textAlignHorizontal?: 'LEFT' | 'CENTER' | 'RIGHT' | undefined
+  textAlignVertical?: 'TOP' | undefined
+  letterSpacing?: number | undefined
+  lineHeightPx?: number | undefined
+  lineHeightPercent?: number | undefined
+  lineHeightUnit?: 'INTRINSIC_%' | undefined
 }
 
 interface CreateElement {
   elementType: string
   content: string
   className?: string
+  styles?: string
 }
 
 type NodeType =
@@ -105,24 +105,52 @@ type NodeType =
     'COMPONENT_SET' |
     'COMPONENT'
 
-function createElement ({ elementType, content, className }: CreateElement): string {
-  return `<${elementType} class="${className ?? ''}">${content}</${elementType}>`
+function normalizeColor (color: Color): string {
+  return `rgb(${String(Math.round(color.r * 255))},${String(Math.round(color.g * 255))},${String(Math.round(color.b * 255))});`
+}
+
+function getStyles (node: Node): string {
+  return `${typeof node.layoutMode === 'string' ? 'display:flex;' : ''}
+  ${node.layoutMode === 'VERTICAL' ? 'flex-direction:column;' : ''}
+  ${node.layoutAlign === 'STRETCH' ? 'align-self:stretch;' : ''}
+  ${node.layoutGrow === 0 ? 'flex-grow: 0;' : ''}
+  ${typeof node.itemSpacing === 'number' ? 'gap:'.concat(String(node.itemSpacing), 'px;') : ''}
+  ${(node.backgroundColor != null) ? 'color:'.concat(normalizeColor(node.backgroundColor)) : ''}
+  ${node.primaryAxisAlignItems === 'SPACE_BETWEEN' ? 'justify-content: space-between;' : ''}
+  ${node.counterAxisAlignItems === 'CENTER' ? 'align-items: center;' : ''}
+  ${node.primaryAxisAlignItems === 'CENTER' ? 'justify-content: center;' : ''}
+  ${(node.paddingTop != null) ? 'padding-top:'.concat(String(node.paddingTop), 'px;') : ''}
+  ${(node.paddingBottom != null) ? 'padding-bottom:'.concat(String(node.paddingBottom), 'px;') : ''}
+  ${(node.paddingLeft != null) ? 'padding-left:'.concat(String(node.paddingLeft), 'px;') : ''}
+  ${(node.paddingRight != null) ? 'padding-right:'.concat(String(node.paddingRight), 'px;') : ''}
+  ${typeof node.style?.fontFamily === 'string' ? 'font-family:'.concat(node.style.fontFamily, ';') : ''}
+  ${typeof node.style?.fontWeight === 'number' ? 'font-weight:'.concat(String(node.style.fontWeight), ';') : ''}
+  ${typeof node.style?.fontSize === 'number' ? 'font-size:'.concat(String(node.style.fontSize), 'px;') : ''}
+  ${typeof node.style?.textAlignHorizontal === 'string' ? 'text-align:'.concat(node.style.textAlignHorizontal.toLowerCase(), ';') : ''}
+  ${typeof node.style?.lineHeightPercent === 'number' ? 'line-height:'.concat(String(node.style.lineHeightPercent), '%;') : ''}
+  ${(node.background != null) && node.background?.length > 0 ? 'background-color:'.concat(normalizeColor(node.background[0].color)) : ''}
+
+  `
+}
+
+function createElement ({ elementType, content, className, styles }: CreateElement): string {
+  return `<${elementType} class="${className ?? ''}" style="${styles ?? ''}">${content}</${elementType}>`
 }
 
 function reducer (node: Node): string {
   switch (node.type) {
     case 'DOCUMENT':
-      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase(), styles: getStyles(node) })
     case 'COMPONENT':
       return ''
     case 'CANVAS':
-      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase(), styles: getStyles(node) })
     case 'FRAME':
-      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase(), styles: getStyles(node) })
     case 'TEXT':
-      return createElement({ elementType: 'p', content: node.characters ?? '', className: node.name.toLowerCase() })
+      return createElement({ elementType: 'span', content: node.characters ?? '', className: node.name.toLowerCase(), styles: getStyles(node) })
     default:
-      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase(), styles: getStyles(node) })
   }
 }
 
