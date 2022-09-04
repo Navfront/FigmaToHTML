a4b37ccb81c1905bd19d98ee317e76aed64da925
Navfront <b3rt@yandex.ru>
2022-09-04 21:06:13 +0300

    Построил начальное дерево элементов

--- solution.js
+++ solution.js
@@ -1,4 +1,47 @@
 "use strict";
-module.exports = (json) => {
-    return '1234';
-};
+function createElement({ elementType, content, className }) {
+    return `<${elementType} class="${className !== null && className !== void 0 ? className : ''}">${content}</${elementType}>`;
+}
+function reducer(node) {
+    var _a, _b, _c, _d, _e;
+    switch (node.type) {
+        case 'DOCUMENT':
+            return createElement({ elementType: 'div', content: (_a = node.characters) !== null && _a !== void 0 ? _a : '', className: node.name.toLowerCase() });
+        case 'COMPONENT':
+            return '';
+        case 'CANVAS':
+            return createElement({ elementType: 'div', content: (_b = node.characters) !== null && _b !== void 0 ? _b : '', className: node.name.toLowerCase() });
+        case 'FRAME':
+            return createElement({ elementType: 'div', content: (_c = node.characters) !== null && _c !== void 0 ? _c : '', className: node.name.toLowerCase() });
+        case 'TEXT':
+            return createElement({ elementType: 'p', content: (_d = node.characters) !== null && _d !== void 0 ? _d : '', className: node.name.toLowerCase() });
+        default:
+            return createElement({ elementType: 'div', content: (_e = node.characters) !== null && _e !== void 0 ? _e : '', className: node.name.toLowerCase() });
+    }
+}
+// const st = new Set()
+// function parse (node: Node): void {
+//   st.add(node.characters)
+//   if ((typeof node.children !== 'undefined') && node.children?.length > 0) {
+//     for (const child of node.children) {
+//       st.add(child.characters)
+//       parse(child)
+//     }
+//   }
+// }
+function traverse(node) {
+    if (node.characters == null) {
+        node.characters = '';
+    }
+    if (typeof node.children !== 'undefined' && node.children.length > 0) {
+        for (const child of node.children) {
+            node.characters = node.characters.concat(' ' + traverse(child));
+        }
+    }
+    return reducer(node);
+}
+function build(json) {
+    const result = traverse(json.document);
+    return result;
+}
+module.exports = build;
--- solution.ts
+++ solution.ts
@@ -1,4 +1,91 @@
-
-module.exports = (json: any) => {
-  return '1234'
+interface Json {
+  document: Node
 }
+interface BGColor {
+  r: number
+  g: number
+  b: number
+  a: number
+}
+
+interface Node {
+  id: string
+  name: string
+  type: NodeType
+  children: Node[] | undefined
+  backgroundColor: BGColor
+  characters?: string | undefined
+  style?: {} | undefined
+  background?: [] | undefined
+  blendMode?: 'PASS_THROUGH' | undefined
+  layoutAlign?: 'INHERIT' | 'STRETCH' | undefined
+  layoutGrow?: 0 | undefined
+  layoutMode?: 'VERTICAL' | 'HORIZONTAL' | undefined
+
+}
+
+interface CreateElement {
+  elementType: string
+  content: string
+  className?: string
+}
+
+type NodeType =
+    'DOCUMENT' |
+    'CANVAS' |
+    'FRAME' |
+    'TEXT' |
+    'INSTANCE' |
+    'RECTANGLE' |
+    'COMPONENT_SET' |
+    'COMPONENT'
+
+function createElement ({ elementType, content, className }: CreateElement): string {
+  return `<${elementType} class="${className ?? ''}">${content}</${elementType}>`
+}
+
+function reducer (node: Node): string {
+  switch (node.type) {
+    case 'DOCUMENT':
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+    case 'COMPONENT':
+      return ''
+    case 'CANVAS':
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+    case 'FRAME':
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+    case 'TEXT':
+      return createElement({ elementType: 'p', content: node.characters ?? '', className: node.name.toLowerCase() })
+    default:
+      return createElement({ elementType: 'div', content: node.characters ?? '', className: node.name.toLowerCase() })
+  }
+}
+
+// const st = new Set()
+// function parse (node: Node): void {
+//   st.add(node.characters)
+//   if ((typeof node.children !== 'undefined') && node.children?.length > 0) {
+//     for (const child of node.children) {
+//       st.add(child.characters)
+//       parse(child)
+//     }
+//   }
+// }
+
+function traverse (node: Node): string {
+  if (node.characters == null) {
+    node.characters = ''
+  }
+  if (typeof node.children !== 'undefined' && node.children.length > 0) {
+    for (const child of node.children) {
+      node.characters = node.characters.concat(' ' + traverse(child))
+    }
+  }
+  return reducer(node)
+}
+
+function build (json: Json): string {
+  const result = traverse(json.document)
+  return result
+}
+module.exports = build
